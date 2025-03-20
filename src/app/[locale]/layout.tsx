4166fd893348bc11c27c0459a92580ce5bda17f4
Artur <167545086+artur0414@users.noreply.github.com>
2025-03-19 21:03:27 -0500

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -63,8 +63,8 @@ export default async function LocaleLayout({
           <Menu />
         </NextIntlClientProvider>
         <Cursor />
+        <Analytics /> 
       </body>
-      <Analytics /> 
     </html>
   );
 }
